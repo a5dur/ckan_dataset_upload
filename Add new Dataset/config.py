1ef676f1cde0b6b10034022503f4b4c294209924
a5dur <mabdurrahman303@gmail.com>
2025-08-25 02:17:38 +0530

    add files

---
+++ Add new Dataset/config.py
@@ -0,0 +1,7 @@
+Config = {
+    'metadata_filepath': 'ckanext_metadata-2.csv',
+    'ckan': {
+        'url': 'https://catalog.civicdataecosystem.org',
+        'api_key': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzWEJTVmxULVpyMmMzMUdhekdGTTVSNGtyYUlCSTYxWHhlNlJNVW04TkZ4VkNGemw0RFN4N3hmZ3pETDdxWTByMUtFU1ZMZHJMcDV4cEpfbyIsImlhdCI6MTc0NDY1NjYwNn0.ysTdJEOD_D_SI-GWerHrEzAK-qEBB5unIiT-a6menSQ'
+    }
+}
